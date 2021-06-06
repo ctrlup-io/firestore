4cb35abdbb8b96407d71b78f0069dd10c78c39ed
Valentin Barit <valentin.barit@gmail.com>
2021-06-07 01:10:07 +0200

    feat: 🎸 expose useFirestoreQuery

--- rollup.config.js
+++ rollup.config.js
@@ -11,6 +11,7 @@ const config = [
       index: 'src/index.js',
       useFirestore: 'src/useFirestore.js',
       useFirestoreGetOne: 'src/useFirestoreGetOne.js',
+      useFirestoreQuery: 'src/useFirestoreQuery.js',
     },
     output: [
       {
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { default as useFirestore } from './useFirestore';
 export { default as useFirestoreGetOne } from './useFirestoreGetOne';
+export { default as useFirestoreQuery } from './useFirestoreQuery';
